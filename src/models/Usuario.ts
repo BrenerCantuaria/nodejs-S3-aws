03d9d57f62d7cb30dbed7225557bc85cbb221050
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-07-07 19:25:23 -0400

    feat:  melhora a validação dos campos do Schema, tornado obrigátorio informar nome, sobrenome, e email único.

--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -2,11 +2,17 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const usuario = new Schema({
-  nome: String,
-  sobrenome: String,
-  email: String,
-  senha: String,
-  cargo: String,
+  nome: { type: String, required: true },
+  sobrenome: { type: String, required: true },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+    lowercase: true, 
+    trim: true, 
+  },
+  senha: { type: String, required: true },
+  cargo: { type: String, required: true },
   createAt: { type: Date, default: Date.now },
 });
 
