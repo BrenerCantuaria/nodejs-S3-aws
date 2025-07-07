91907ac5d19e2c0322964424e037d6e30f39d7dc
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-07-07 19:18:03 -0400

    feat: definindo as rotas do usuário

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { pdfRoutes } from "./pdf.routes";
+import { usuarioRoutes } from "./usuarios.routes";
 
 export async function registerRoutes(app: FastifyInstance) {
   await pdfRoutes(app);
-
-  // No futuro: await userRoutes(app);
+  await usuarioRoutes(app);
   // await authRoutes(app);
 }
---
+++ src/routes/usuarios.routes.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance } from "fastify";
+import {
+  buscaUsuarioController,
+  criaUsuarioController,
+  deletaUsuarioController,
+} from "../controllers/usuario.controller";
+export async function usuarioRoutes(app: FastifyInstance) {
+  app.post("/usuario", criaUsuarioController);
+  app.get("/usuario/:id", buscaUsuarioController);
+  app.post("/usuario/:id", deletaUsuarioController);
+}
