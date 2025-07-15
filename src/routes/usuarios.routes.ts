98e98c0ead28d9b91362e2cb8f9c09e25c628738
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-07-15 16:47:14 -0400

    feat: adicionando a rota de /registrar e /login para tornar possivel o cadastro de novos usuarios.

--- src/routes/usuarios.routes.ts
+++ src/routes/usuarios.routes.ts
@@ -3,15 +3,25 @@ import {
   buscaUsuarioController,
   criaUsuarioController,
   deletaUsuarioController,
+  loginUsuarioController,
 } from "../controllers/usuario.controller";
-import { LoginRequired } from "../middlewares/login";
+import { LoginRequired } from "../middlewares/loginRequired";
 import { Usuario } from "../types/usuario";
 import { ObjectId } from "mongodb";
 
 export async function usuarioRoutes(app: FastifyInstance) {
   app.post<{
     Body: Omit<Usuario, "createAt">;
-  }>("/usuario", { preHandler: LoginRequired }, criaUsuarioController);
-  app.get<{ Params: { id: ObjectId } }>("/usuario/:id", { preHandler: LoginRequired }, buscaUsuarioController);
-  app.delete<{ Params: { id: ObjectId } }>("/usuario/:id", { preHandler: LoginRequired }, deletaUsuarioController);
+  }>("/registrar", criaUsuarioController);
+  app.post("/login", loginUsuarioController);
+  app.get<{ Params: { id: ObjectId } }>(
+    "/usuario/:id",
+    { preHandler: LoginRequired },
+    buscaUsuarioController
+  );
+  app.delete<{ Params: { id: ObjectId } }>(
+    "/usuario/:id",
+    { preHandler: LoginRequired },
+    deletaUsuarioController
+  );
 }
