25bbb0e88d4044e75d437f1e1fdee9c314bac04d
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-07-15 17:46:44 -0400

    feat: registra o fastifyjwt no servidor

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,17 @@ import fastifyMultipart from "@fastify/multipart";
 import { fastify } from "fastify";
 import { connectToServer } from "./database/config";
 import { registerRoutes } from "./routes/routes";
+import fastifyJwt from "@fastify/jwt";
 const server = fastify({ logger: true });
 server.register(fastifyMultipart);
 connectToServer();
+
+
+server.register(fastifyJwt,{
+  secret: process.env.JWT_SECRET || ""
+})
+
+
 
 // rotas da aplição
 registerRoutes(server);
