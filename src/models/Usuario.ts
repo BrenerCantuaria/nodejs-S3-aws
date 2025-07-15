6b7ef5b6e91e95bfa027c725725698498604dcb5
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-07-15 16:50:43 -0400

    feat: adiciona interface  UsuarioDoc para retornar os tipos do Schema e implementa função pre  "save" para encriptar senha.

--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -1,20 +1,46 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-const { Schema } = mongoose;
 
-const usuario = new Schema({
+interface UsuarioDoc extends Document {
+  nome: string;
+  sobrenome: string;
+  email: string;
+  senha: string;
+  cargo: string;
+  createAt: Date;
+  arquivos: string[];
+  compararSenha(senhaFornecida: string): Promise<boolean>;
+}
+
+const { Schema } = mongoose;
+const usuario = new Schema<UsuarioDoc>({
   nome: { type: String, required: true },
   sobrenome: { type: String, required: true },
-  email: { 
-    type: String, 
-    required: true, 
-    unique: true, 
-    lowercase: true, 
-    trim: true, 
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
   },
   senha: { type: String, required: true },
   cargo: { type: String, required: true },
   createAt: { type: Date, default: Date.now },
-  arquivos: [{type: Schema.Types.ObjectId, ref: 'pdfs'}]
+  arquivos: [{ type: Schema.Types.ObjectId, ref: "pdfs" }],
 });
 
-export const usuarioMoongoseSchema = mongoose.model("usuarios", usuario);
+usuario.pre("save", async function (next) {
+  if (!this.isModified("senha")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.senha = await bcrypt.hash(this.senha, salt);
+  } catch (error) {
+    console.log("Erro ao tentar encriptar a senha do usuario", error);
+  }
+});
+
+usuario.methods.compararSenha = async function (senhaFornecida: string) {
+  return await bcrypt.compare(senhaFornecida, this.senha);
+};
+
+export const usuarioMoongoseSchema = mongoose.model<UsuarioDoc>("usuarios", usuario);
