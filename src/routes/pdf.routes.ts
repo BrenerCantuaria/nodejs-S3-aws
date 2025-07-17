71e52e313b3c631b2d258e7155e4c7e1d992836a
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-07-17 16:28:21 -0400

    feat: adicionando middleware para validar se o usuário está logado.

--- src/routes/pdf.routes.ts
+++ src/routes/pdf.routes.ts
@@ -1,8 +1,16 @@
 import { FastifyInstance } from "fastify";
-import { uploadPdfHandler, listaPdfsHandler, deletePdfHandler } from "../controllers/pdf.controller";
+import {
+  uploadPdfHandler,
+  listaPdfsHandler,
+  deletePdfHandler,
+  listaPdfsHandlerById,
+} from "../controllers/pdf.controller";
+import { LoginRequired } from "../middlewares/loginRequired";
+import { ObjectId } from "mongodb";
 
 export async function pdfRoutes(app: FastifyInstance) {
-  app.get("/pdf", listaPdfsHandler);
-  app.post("/pdf", uploadPdfHandler);
-  app.delete("/pdf/:id", deletePdfHandler);
+  app.get("/pdf", { preHandler: LoginRequired }, listaPdfsHandler);
+  app.get<{ Params: { id: ObjectId } }>("/pdf/:id", { preHandler: LoginRequired }, listaPdfsHandlerById);
+  app.post("/pdf", { preHandler: LoginRequired }, uploadPdfHandler);
+  app.delete<{ Params: { id: ObjectId } }>("/pdf/:id", { preHandler: LoginRequired },deletePdfHandler);
 }
